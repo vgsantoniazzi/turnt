b4d2d33d8ced362219a93be62c27ff8d5a1e1cc6
Victor Antoniazzi <vgsantoniazzi@gmail.com>
2014-02-27 21:13:52 -0300

    Add basic tests for gem turnt

--- test/test_turnt.rb
+++ test/test_turnt.rb
@@ -1,7 +1,33 @@
 require 'helper'
 
 class TestTurnt < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def test_has_name
+    organization = Organization.new
+    assert_equal true, organization.has_name?
+  end
+
+  def test_has_partners
+    organization = Organization.new
+    assert_equal false, organization.has_partners?
+  end
+
+  def test_method_missing_without_has
+    organization = Organization.new
+    assert_raise(NoMethodError) { organization.ha_partners? }
+  end
+
+  def test_method_missing_with_has
+    organization = Organization.new
+    assert_raise(NoMethodError) { organization.has_invalid_attribute? }
+  end
+end
+
+class Organization
+  include Turnt
+  def name
+    "fuu bazz"
+  end
+
+  def partners
   end
 end
